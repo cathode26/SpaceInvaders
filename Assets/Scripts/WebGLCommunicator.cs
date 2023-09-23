f7f09483cc2ed76d31c71aa2e2690744170f36f1
cathode26 <Vincent.J.Liguori@outlook.com>
2023-09-22 21:09:01 -0400

    Added WebGLCommunicator to pause/unpause game from web

---
+++ Assets/Scripts/WebGLCommunicator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class WebGLCommunicator : MonoBehaviour
+{
+    public void Awake()
+    {
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+            Time.timeScale = 0;
+    }
+    public void ShowPlayer(string show)
+    {
+        // Convert the string to a bool value
+        bool showBool = show.ToLower() == "true";
+
+        if (showBool)
+            Time.timeScale = 1;
+        else
+            Time.timeScale = 0;
+
+        Debug.Log("ShowPlayer called with value: " + show);
+    }
+}
